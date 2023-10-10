1539c0eafb9d0e4586aaacc258cdf24890146226
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-10 20:09:45 +0300

    initial files

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Link href="/">Home Page</Link>
+        <Link href="/hello">Show Page</Link>
+        <slot></slot>
+        <div>{{ timer }}</div>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue';
+
+const timer = ref(0);
+
+setInterval(() => timer.value++, 1000);
+
+</script>
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Index</h1>
+    <Link href="/hello">Show Page</Link>
+    <div>{{ message }}</div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    message: String,
+});
+</script>
+
+<script>
+import MainLayout from '@/Layouts/MainLayout.vue';
+
+export default {
+    layout: MainLayout,
+    
+};
+</script>
+
