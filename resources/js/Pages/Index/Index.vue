c4dbb559db4765069da71966cdd28975ab1fc9e0
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-11 17:35:27 +0300

    added Listing model, connected mysql and created fake data

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -1,23 +1,15 @@
 <template>
-    <h1>Index</h1>
-    <Link href="/hello">Show Page</Link>
-    <div>{{ message }}</div>
+  <h1>Index</h1>
+  <Link href="/hello">Show Page</Link>
+  <div>{{ message }}</div>
 </template>
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3';
+import { Link } from '@inertiajs/inertia-vue3'
 
 defineProps({
-    message: String,
-});
+  message: String,
+})
 </script>
 
-<script>
-import MainLayout from '@/Layouts/MainLayout.vue';
-
-export default {
-    layout: MainLayout,
-    
-};
-</script>
 
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,20 +1,12 @@
 <template>
-        <h1>Show</h1>
-        <Link href="/">Home Page</Link>
-           
+  <h1>Show</h1>
+  <Link href="/">Home Page</Link>
 </template>
 
 
 <script setup>
-import { Link } from '@inertiajs/inertia-vue3';
+import { Link } from '@inertiajs/inertia-vue3'
 
 </script>
 
-<script>
-import MainLayout from '@/Layouts/MainLayout.vue';
 
-export default {
-    layout: MainLayout,
-
-};
-</script>
