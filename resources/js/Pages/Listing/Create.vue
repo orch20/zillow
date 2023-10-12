f3cd4d3dcdf0ce01770bc116e141ec26d5612c29
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-12 18:31:03 +0300

    simplified routes names

--- resources/js/Pages/Listing/Create.vue
+++ resources/js/Pages/Listing/Create.vue
@@ -81,7 +81,7 @@ const form = useForm({
     street_nr: null,
     price: 0,
 })
-const create = () => form.post('/listing')
+const create = () => form.post(route('listing.store'))
 </script>
 
 <style scoped>
