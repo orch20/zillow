25305920b656d0c6a8641902e58de7b47a48cd90
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-14 20:11:23 +0300

    deleted garbage

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,3 @@ use App\Http\Controllers\ListingController;
 Route::get('/', [IndexController::class, 'index']);
 Route::get('/hello', [IndexController::class, 'show']);
 Route::resource('listing', ListingController::class);
-// ->only(['index', 'show', 'create', 'store']);
