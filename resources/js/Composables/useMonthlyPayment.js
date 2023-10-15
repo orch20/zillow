15db9486d52dd78fde8f1499ffb88719a331dd0b
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-15 20:55:18 +0300

    refactored composible useMonthlyPayment to get more data

--- resources/js/Composables/useMonthlyPayment.js
+++ resources/js/Composables/useMonthlyPayment.js
@@ -1,21 +1,32 @@
-import { computed, isRef } from 'vue'
-
-export const useMonthlyPayment = (price, interestRate, duration) => {
+import { computed, isRef } from "vue";
+export const useMonthlyPayment = (total, interestRate, duration) => {
     const monthlyPayment = computed(() => {
-        const principle = isRef(price) ? price.value : price
+        const principle = isRef(total) ? total.value : total;
         const monthlyInterest =
             (isRef(interestRate) ? interestRate.value : interestRate) /
             100 /
-            12
-        const numberOfPaymentMonths = isRef(duration)
-            ? duration.value
-            : duration
+            12;
+        const numberOfPaymentMonths =
+            (isRef(duration) ? duration.value : duration) * 12;
         return (
             (principle *
                 monthlyInterest *
                 Math.pow(1 + monthlyInterest, numberOfPaymentMonths)) /
             (Math.pow(1 + monthlyInterest, numberOfPaymentMonths) - 1)
-        )
-    })
-    return { monthlyPayment }
-}
+        );
+    });
+
+    const totalPaid = computed(() => {
+        return (
+            (isRef(duration) ? duration.value : duration) *
+            12 *
+            monthlyPayment.value
+        );
+    });
+
+    const totalInterest = computed(
+        () => totalPaid.value - (isRef(total) ? total.value : total)
+    );
+
+    return { monthlyPayment, totalPaid, totalInterest };
+};
