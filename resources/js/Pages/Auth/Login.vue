63c9bae58d144ea6c2a62bda637b057e36557504
orch20 <94862153+orch20@users.noreply.github.com>
2023-10-16 19:09:37 +0300

    added login controller and routes & login form markup, sign in logic

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div  class="w-1/2 mx-auto">
+    <form @submit.prevent="login">
+            <div>
+                <label for="email" class="label">Email (username)</label>
+                <input type="email" name="email" id="email" class="input" v-model="form.email" />
+                <div class="input-error">Potential errors</div>
+            </div>
+            <div class="mt-4">
+                    <label for="password" class="label">Password</label>
+                    <input type="password" name="password" id="password" class="input" v-model="form.password" />
+                </div>
+                <div class="mt-4">
+                    <button class="btn-primary w-full" type="submit">Login</button>
+                </div>
+        </form>
+    </div>
+    
+</template>
+
+<script setup>
+import { useForm } from '@inertiajs/inertia-vue3';
+
+    const form = useForm({
+        email: '',
+        password: '',
+    });
+
+const login = () => form.post(route('login.store'));
+</script>
